5c5dd3d10ca62aec9997a1570145347fafc9f0bd
Mocktar <mocktarissa@gmail.com>
2020-07-02 16:16:37 +0300

    Fixed deployement issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/app'
+import App from './containers/App'
 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
